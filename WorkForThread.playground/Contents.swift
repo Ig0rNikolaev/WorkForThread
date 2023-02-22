71aa4cf972897e7fb41f5a07cd2616b5e0d31206
Igor Nikolaev <igor01131987@gmail.com>
2023-02-22 14:06:38 +0300

    Add code

--- WorkForThread.playground/Contents.swift
+++ WorkForThread.playground/Contents.swift
@@ -22,3 +22,75 @@ public struct Chip {
         sleep(UInt32(soderingTime))
     }
 }
+
+class Repository {
+    private var repository = [Chip]()
+    private let queue = DispatchQueue(label: "queueRepository", attributes: .concurrent)
+    var countRepository: Int {
+        return queue.sync {
+            repository.count
+        }
+    }
+
+    func appendChip(chip: Chip) {
+        queue.async(flags: .barrier) {
+            self.repository.append(chip)
+        }
+    }
+
+    func removeChip() -> Chip {
+        return queue.sync(flags: .barrier) {
+            repository.removeLast()
+        }
+    }
+}
+
+class GeneratingThread: Thread {
+    static let condition = NSCondition()
+    static var isPredicate = false
+    private let repository: Repository
+    private let dateFormatter = DateFormatter()
+
+    public init(repository: Repository) {
+        self.repository = repository
+        dateFormatter.dateFormat = "HH:mm:ss.SSS"
+    }
+
+    override public func main() {
+        (0..<10).forEach { _ in
+            GeneratingThread.condition.lock()
+            repository.appendChip(chip: Chip.make())
+            print("\(dateFormatter.string(from: Date())): \(repository.countRepository) чип создан.")
+            !GeneratingThread.isPredicate
+            GeneratingThread.condition.signal()
+            GeneratingThread.condition.unlock()
+            GeneratingThread.sleep(forTimeInterval: 2)
+        }
+    }
+}
+
+class WorkerThread: Thread {
+    private let repository: Repository
+    private let dateFormatter = DateFormatter()
+
+    public init(repository: Repository) {
+        self.repository = repository
+        dateFormatter.dateFormat = "HH:mm:ss.SSS"
+    }
+
+    override public func main() {
+        (0..<10).forEach { _ in
+            if !GeneratingThread.isPredicate {
+                GeneratingThread.condition.wait()
+            }
+            repository.removeChip().sodering()
+            print("\(dateFormatter.string(from: Date())): \(repository.countRepository) чип припаян.")
+        }
+    }
+}
+
+let repository = Repository()
+let generatingThread = GeneratingThread(repository: repository)
+let workerThread = WorkerThread(repository: repository)
+generatingThread.start()
+workerThread.start()
